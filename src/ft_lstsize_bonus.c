3e2c6f7884431b575b3d295b21d96bfcaeb54b81
Malu Araújo <araujomaluc@gmail.com>
2025-07-29 00:11:58 -0300

    feat: some list bonus functions

---
+++ src/ft_lstsize_bonus.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		size;
+	
+	if (!lst)
+		return (0);
+	size = 0;
+	while (lst != NULL)
+	{
+		lst = lst -> next;
+		size++;
+	}
+	return (size);
+}
